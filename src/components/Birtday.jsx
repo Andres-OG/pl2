6a611791329dc437867630af0c7aad183f474cdf
Andres OG <andresog@192.168.1.7>
2024-05-31 23:45:18 -0600

    31/Mayo/2024-5

--- src/components/Birtday.jsx
+++ src/components/Birtday.jsx
@@ -5,7 +5,7 @@ const BirthdayAnimation = () => {
     return (
         <div className="flex justify-center items-center p-5 bg-white">
             <div className="text-center flex gap-3 justify-center items-center">
-                <h1 className="text-4xl font-bold mb-4 font-sans animated-text">¡Feliz Cumpleaños!</h1>
+                <h1 className="text-4xl font-bold mb-4 font-sans animated-text">¡Feliz Cumpleaños, preciosa!</h1>
             </div>
         </div>
     );
