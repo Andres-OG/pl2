c569f95cfde62d80b60c793c35d9d397a2431175
Andres OG <andresog@192.168.1.7>
2024-05-31 23:24:18 -0600

    31/Mayo/2024-3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  homepage: ' https://andres-og.github.io/pl2'
+  base: "https://andres-og.github.io/pl2"
 })
